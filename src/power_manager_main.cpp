bea1836fc22f176a1b6ddbd890bbd0ee8cd5a260
Praveen Subramaniyam <psubramaniya@nvidia.com>
2021-12-14 07:38:56 -0800

    Adding Nvidia Power Manager Skeleton files

---
+++ src/power_manager_main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+////////////////////////////////////////////////////////////////////////////////
+/**
+ * @brief Service Entry Point
+ */
+int main(int argc, char* argv[])
+{
+    cout << "Hello from Nvidia Power Manager.\n";
+    return 0;
+}
